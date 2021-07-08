a2788392f66cd64e9019741ce10cf9c1d6b26632
evaseemefly <evaseemefly@126.com>
2021-07-08 22:24:31 +0800

    title:21-07-08 修改 CreateCaseForm.vue ,加入 select 但引发了bug未解决

--- src/components/form/CreateCaseForm.vue
+++ src/components/form/CreateCaseForm.vue
@@ -3,7 +3,26 @@
         <div class="base-card">
             <div class="base-card-title"><h4>台风信息</h4></div>
             <div class="base-card-content">
-                <div class="base-card-row"><p>台风编号</p></div>
+                <div class="base-card-row">
+                    <p>台风编号</p>
+                    <el-select v-model="value" placeholder="请选择">
+                        <el-option
+                            v-for="item in tyCodeOptions"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value"
+                        >
+                        </el-option>
+                    </el-select>
+                    <!-- <el-select placeholder="请选择" v-model="form.oilType">
+                        <el-option
+                            v-for="item in optionOilType"
+                            :key="item.key"
+                            :label="item.name"
+                            :value="item.key"
+                        ></el-option>
+                    </el-select> -->
+                </div>
             </div>
         </div>
         <div class="base-card">
@@ -19,12 +38,14 @@
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
 @Component({})
 export default class CreateCaseForm extends Vue {
-    mydata: any = null
+    tyCodeOptions: { label: string; value: string }[] = [
+        { label: '101', value: '101' },
+        { label: '102', value: '102' },
+        { label: '103', value: '103' }
+    ]
+    value = ''
     @Prop()
     isShow: boolean
-    get computedTest() {
-        return null
-    }
     @Watch('isShow')
     onIsShow(val: boolean): void {
         const divCreateForm = document.getElementById('base_form_createcast')
@@ -52,8 +73,13 @@ export default class CreateCaseForm extends Vue {
     // 解决加入了圆角但导致子元素溢出的问题
     overflow: hidden;
     // 加入阴影
-    box-shadow: 7px 8px 10px 1px #888888;
+    box-shadow: 7px 8px 10px 1px #323134;
     width: 600px;
+    // visibility: visible;
+    position: absolute;
+    top: 50%;
+    left: 30%;
+    z-index: 9999;
     .base-card {
         // padding: 10px;
     }
@@ -64,10 +90,17 @@ export default class CreateCaseForm extends Vue {
     }
     .base-card-content {
         padding: 15px;
-        font-size: large;
+        font-size: larger;
+        font-weight: 500;
+        line-height: 1.5;
+        color: #212529;
+        text-align: left;
         .base-card-row {
             line-height: 2.5rem;
         }
     }
+}
+.base-card-row {
+    display: flex;
 }
 </style>
