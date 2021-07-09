0e75f2e9656287baba31cb534d2a6658599bd835
evaseemefly by 7530 <evaseemefly@126.com>
2021-07-09 15:54:43 +0800

    title:21-07-09 基本完成了 CreateCaseForm.vue 并加入了动态添加误差半径范围值的逻辑

--- src/components/form/CreateCaseForm.vue
+++ src/components/form/CreateCaseForm.vue
@@ -5,7 +5,16 @@
             <div class="base-card-content">
                 <div class="base-card-row">
                     <p>台风编号</p>
-                    <el-select v-model="value" placeholder="请选择">
+                    <!-- <el-select v-model="value" placeholder="请选择">
+                        <el-option
+                            v-for="item in options"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value"
+                        >
+                        </el-option>
+                    </el-select> -->
+                    <el-select v-model="tyCode" clearable placeholder="请选择">
                         <el-option
                             v-for="item in tyCodeOptions"
                             :key="item.value"
@@ -26,10 +35,33 @@
             </div>
         </div>
         <div class="base-card">
-            <div class="base-card-title"><h4>高级</h4></div>
+            <div class="base-card-title">
+                <h4>高级</h4>
+            </div>
             <div class="base-card-content">
-                <div class="base-card-row">成员数量</div>
-                <div class="base-card-row">误差半径增减</div>
+                <div class="base-card-row">
+                    成员数量
+                    <el-input-number
+                        v-model="membersNum"
+                        :min="1"
+                        label="描述文字"
+                    ></el-input-number>
+                </div>
+                <div class="base-card-row">
+                    误差半径增减
+                    <el-input-number
+                        v-model="deviationRadiusNum"
+                        :min="deviationRadiusLenMin"
+                        label="描述文字"
+                        @change="deviationChange"
+                    ></el-input-number>
+                </div>
+                <div class="base-card-row">
+                    <div class="cell" :key="item.key" v-for="item in deviationRadiusList">
+                        <p>{{ item.hours }}h</p>
+                        <el-input v-model="item.radius" placeholder="请输入内容"></el-input>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -43,9 +75,38 @@ export default class CreateCaseForm extends Vue {
         { label: '102', value: '102' },
         { label: '103', value: '103' }
     ]
-    value = ''
+    deviationRadiusList: { hours: number; radius: number }[] = [
+        { hours: 24, radius: 60 },
+        { hours: 48, radius: 100 },
+        { hours: 72, radius: 120 },
+        { hours: 96, radius: 150 }
+    ]
+    deviationRadiusLenMin = 4
+    tyCode = ''
+    membersNum = 0
+    deviationRadiusNum = 4
     @Prop()
     isShow: boolean
+
+    deviationChange(num: number, oldNum: number): void {
+        const hoursInterval = 24
+        const radiusInterval = 30
+        const lastDeviation = this.deviationRadiusList.slice(-1)[0]
+        if (num > oldNum) {
+            // +
+            const newDeviationHours = lastDeviation.hours + hoursInterval
+            const newDeviationRadius = lastDeviation.radius + radiusInterval
+            const newDeviation = { hours: newDeviationHours, radius: newDeviationRadius }
+            this.deviationRadiusList.push(newDeviation)
+            console.log(newDeviation)
+        } else if (num < oldNum) {
+            if (this.deviationRadiusList.length > this.deviationRadiusLenMin) {
+                this.deviationRadiusList.pop()
+            }
+        }
+        console.log(`new:${num},old:${oldNum}`)
+    }
+
     @Watch('isShow')
     onIsShow(val: boolean): void {
         const divCreateForm = document.getElementById('base_form_createcast')
@@ -61,6 +122,10 @@ export default class CreateCaseForm extends Vue {
                 }
             }
         }
+    }
+
+    get getDeviationRadiusNum(): number {
+        return this.deviationRadiusList.length
     }
 }
 </script>
@@ -79,7 +144,7 @@ export default class CreateCaseForm extends Vue {
     position: absolute;
     top: 50%;
     left: 30%;
-    z-index: 9999;
+    z-index: 999;
     .base-card {
         // padding: 10px;
     }
@@ -97,10 +162,18 @@ export default class CreateCaseForm extends Vue {
         text-align: left;
         .base-card-row {
             line-height: 2.5rem;
+            justify-content: space-around;
         }
     }
 }
 .base-card-row {
     display: flex;
+    .cell {
+        display: flex;
+        margin: 5px;
+    }
+    .cell > p {
+        margin-right: 8px;
+    }
 }
 </style>
