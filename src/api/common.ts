8e89a8c54cbd98ea9794c3d28a6409317e0dc9a7
evaseemeflt-t7920 <evaseemefly@126.com>
2022-06-10 08:32:11 +0800

    title:22-06-10 7920的api相关配置

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,11 +1,11 @@
 // TODO:[-] 20-02-07 将所有的共有的常量放在common.ts中
-export const host = 'http://127.0.0.1:8000'
+// export const host = 'http://127.0.0.1:8000'
 // mac
 // export const host = 'http://127.0.0.1:8082'
 // home 5820
 // export const host = 'http://127.0.0.1:8082'
 // - 21-10-13 上线测试的实际地址,注意要去掉/
-// export const host = 'http://128.5.10.21:8000'
+export const host = 'http://128.5.10.21:8000'
 // 22-06-05 尝试加入家中 5820 发布服务的配置
 // export const host = 'http://192.168.50.86:8082'
 
